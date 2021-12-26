379ce31c09964674b95347c5c895b92fd25841c0
LeeJaeHoon <dlwogns3413@naver.com>
2021-12-27 00:58:10 +0900

    feat: adapating and extending

--- src/App.js
+++ src/App.js
@@ -4,23 +4,21 @@ const Fater = styled.div`
   display: flex;
 `;
 
-const BoxOne = styled.div`
-  background-color: tomato;
+const Box = styled.div`
+  background-color: ${props => props.bgColor};
   width: 100px;
   height: 100px;
 `;
 
-const BoxTwo = styled.div`
-  background-color: aqua;
-  width: 100px;
-  height: 100px;
+const Circle = styled(Box)`
+  border-radius: 50px;
 `;
 
 function App() {
   return (
     <Fater>
-      <BoxOne />
-      <BoxTwo />
+      <Box bgColor="teal" />
+      <Circle bgColor="tomato" />
     </Fater>
   );
 }
