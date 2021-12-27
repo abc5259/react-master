9b599308a222528c535656d8eb5f4ad1b4c3067e
LeeJaeHoon <dlwogns3413@naver.com>
2021-12-27 14:03:22 +0900

    feat: learning typescript with react

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,12 @@
 import styled, { keyframes } from "styled-components";
-
-const Wrapper = styled.div`
-  display: flex;
-  height: 100vh;
-  align-items: center;
-  justify-content: center;
-  background-color: ${props => props.theme.backgroundColor};
-`;
-
-const rotationAnimation = keyframes`
-  0% {
-    transform: rotate(0)
-  }
-  50% {
-    transform: rotate(360deg)
-  }
-  100% {
-    transform: rotate(0)
-  }
-`;
-
-const Title = styled.span`
-  font-size: 100px;
-  color: ${props => props.theme.textColor};
-`;
-
-const Box = styled.div`
-  width: 200px;
-  height: 200px;
-  background-color: teal;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  animation: ${rotationAnimation} 3s linear infinite;
-  ${Title} {
-    &:hover {
-      font-size: 40px;
-    }
-  }
-`;
+import Circle from "./Circle";
 
 function App() {
   return (
-    <Wrapper>
-      <Box>
-        <Title>hi</Title>
-      </Box>
-    </Wrapper>
+    <div>
+      <Circle bgColor="teal" />
+      <Circle bgColor="tomato" />
+    </div>
   );
 }
 
