70caeffb5a84e6d30f303904a07270ce1b981361
LeeJaeHoon <dlwogns3413@naver.com>
2021-12-29 00:44:37 +0900

    feat: learning themeprovider with typescirpt

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,18 @@
-import React, { useState } from "react";
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: ${props => props.theme.bgColor};
+`;
+
+const H1 = styled.h1`
+  color: ${props => props.theme.textColor};
+`;
 
 function App() {
-  const [value, setValue] = useState("");
-
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setValue(value);
-  };
-
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(value);
-  };
-
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={onChange}
-          type="text"
-          placeholder="usenName"
-        />
-        <button>Log in</button>
-      </form>
-    </div>
+    <Container>
+      <H1>awdaw</H1>
+    </Container>
   );
 }
 
