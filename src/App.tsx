0ab99d511d8d494d7adb7a8f0199ba8a46e21429
LeeJaeHoon <dlwogns3413@naver.com>
2021-12-29 00:15:23 +0900

    feat: event with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
+import React, { useState } from "react";
 import styled, { keyframes } from "styled-components";
-import Circle from "./Circle";
 
 function App() {
+  const [value, setValue] = useState("");
+
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(value);
+  };
+
   return (
     <div>
-      <Circle bgColor="teal" />
-      <Circle bgColor="tomato" borderColor="aqua" />
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="usenName"
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
