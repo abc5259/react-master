e7bd411cdad96140ed66cc39f82fc2dfd7ed580b
LeeJaeHoon <dlwogns3413@naver.com>
2022-01-01 01:16:22 +0900

    clear Coin Project

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { fetchCoins } from "../api";
 import { Helmet } from "react-helmet";
+import { useSetRecoilState } from "recoil";
+import { isDarkAtom } from "../atoms";
 
 const Container = styled.div`
   padding: 0 20px;
@@ -67,6 +69,8 @@ interface ICoin {
 
 const Coins = () => {
   const { isLoading, data } = useQuery<ICoin[]>("allCoins", fetchCoins);
+  const setDarkAtom = useSetRecoilState(isDarkAtom);
+  const toggleDarkAtom = () => setDarkAtom(prev => !prev);
   return (
     <Container>
       <Helmet>
@@ -74,6 +78,7 @@ const Coins = () => {
       </Helmet>
       <Header>
         <Title>코인</Title>
+        <button onClick={toggleDarkAtom}>Toggle Mode</button>
       </Header>
       <CoinsList>
         {isLoading ? (
