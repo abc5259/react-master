1f5b20861c9342d732ba13278cf5962b2af1078d
LeeJaeHoon <dlwogns3413@naver.com>
2021-12-31 15:16:20 +0900

    feat: chart component

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,4 +1,15 @@
-const Chart = () => {
+import { useQuery } from "react-query";
+import { fetchCoinHistory } from "../api";
+
+interface ChartProps {
+  coinId: string;
+}
+
+const Chart = ({ coinId }: ChartProps) => {
+  const { isLoading, data } = useQuery(["ohlcv", coinId], () =>
+    fetchCoinHistory(coinId)
+  );
+  console.log(data);
   return <div>Chart</div>;
 };
 
