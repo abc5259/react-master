24793f1c3a110e62e33cb9b5eeea265d2ecad868
LeeJaeHoon <dlwogns3413@naver.com>
2022-01-02 23:33:43 +0900

    feat: ToDo Component onClick event clear

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,12 +1,33 @@
-import { IToDo } from "../atoms";
+import React from "react";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "../atoms";
 
 const ToDo = ({ text, catagory, id }: IToDo) => {
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+  };
+
   return (
     <li>
       <span>{text}</span>
-      <button>To Do</button>
-      <button>Doing</button>
-      <button>Done</button>
+      {catagory !== "DOING" && (
+        <button name="DOING" onClick={onClick}>
+          Doing
+        </button>
+      )}
+      {catagory !== "TO_DO" && (
+        <button name="TO_DO" onClick={onClick}>
+          To Do
+        </button>
+      )}
+      {catagory !== "DONE" && (
+        <button name="DONE" onClick={onClick}>
+          Done
+        </button>
+      )}
     </li>
   );
 };
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,21 +1,36 @@
 import { useRecoilValue } from "recoil";
+import styled from "styled-components";
 import { toDoState } from "../atoms";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
+const ToDoContainer = styled.div`
+  margin-top: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  h1 {
+    font-size: 32px;
+    margin-bottom: 20px;
+  }
+  ul {
+    margin-top: 20px;
+  }
+`;
+
 const ToDoList = () => {
   const toDos = useRecoilValue(toDoState);
   return (
-    <div>
+    <ToDoContainer>
       <h1>To Dos</h1>
-      <hr />
       <CreateToDo />
       <ul>
         {toDos.map(toDo => (
           <ToDo key={toDo.id} {...toDo} />
         ))}
       </ul>
-    </div>
+    </ToDoContainer>
   );
 };
 
