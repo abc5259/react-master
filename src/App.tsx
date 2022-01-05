a70ea333156684b9a9cff16709a34e89f0bfa034
LeeJaeHoon <dlwogns3413@naver.com>
2022-01-05 17:52:20 +0900

    feat: slider clear

--- src/App.tsx
+++ src/App.tsx
@@ -24,45 +24,53 @@ const Box = styled(motion.div)`
 `;
 
 const boxVariants = {
-  initial: {
-    opacity: 0,
-    x: 300,
-    scale: 0,
+  entry: (back: boolean) => {
+    return {
+      opacity: 0,
+      x: back ? -300 : 300,
+      scale: 0,
+    };
   },
-  visible: {
+  center: {
     opacity: 1,
     x: 0,
     scale: 1,
     transition: { duration: 1 },
   },
-  leaving: {
-    x: -300,
-    opacity: 0,
-    scale: 0,
+  exit: (back: boolean) => {
+    return {
+      opacity: 0,
+      x: back ? 300 : -300,
+      scale: 0,
+    };
   },
 };
 
 function App() {
   const [visible, setVisible] = useState(1);
-  const nextPlease = () => setVisible(prev => (prev === 10 ? 10 : prev + 1));
-  const prevPlease = () => setVisible(prev => (prev === 1 ? 1 : prev - 1));
+  const [back, setBack] = useState(false);
+  const nextPlease = () => {
+    setBack(false);
+    setVisible(prev => (prev === 10 ? 10 : prev + 1));
+  };
+  const prevPlease = () => {
+    setBack(true);
+    setVisible(prev => (prev === 1 ? 1 : prev - 1));
+  };
   return (
     <>
       <Wrapper>
-        <AnimatePresence>
-          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(i =>
-            i === visible ? (
-              <Box
-                variants={boxVariants}
-                initial="initial"
-                animate="visible"
-                exit="leaving"
-                key={i}
-              >
-                {i}
-              </Box>
-            ) : null
-          )}
+        <AnimatePresence custom={back}>
+          <Box
+            custom={back}
+            variants={boxVariants}
+            initial="entry"
+            animate="center"
+            exit="exit"
+            key={visible}
+          >
+            {visible}
+          </Box>
         </AnimatePresence>
         <button onClick={nextPlease}>next</button>
         <button onClick={prevPlease}>prev</button>
