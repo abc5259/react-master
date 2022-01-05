c68f7311a05eb2a206ea406e1ae38754fbb50eb5
LeeJaeHoon <dlwogns3413@naver.com>
2022-01-05 17:23:35 +0900

    feat: make  Slider

--- src/App.tsx
+++ src/App.tsx
@@ -11,49 +11,61 @@ const Wrapper = styled(motion.div)`
 `;
 
 const Box = styled(motion.div)`
-  width: 300px;
+  width: 400px;
   height: 200px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
+  position: absolute;
+  top: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 const boxVariants = {
   initial: {
     opacity: 0,
+    x: 300,
     scale: 0,
   },
   visible: {
     opacity: 1,
+    x: 0,
     scale: 1,
-    rotateZ: 360,
+    transition: { duration: 1 },
   },
   leaving: {
+    x: -300,
     opacity: 0,
     scale: 0,
-    y: 50,
   },
 };
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => {
-    setShowing(show => !show);
-  };
+  const [visible, setVisible] = useState(1);
+  const nextPlease = () => setVisible(prev => (prev === 10 ? 10 : prev + 1));
+  const prevPlease = () => setVisible(prev => (prev === 1 ? 1 : prev - 1));
   return (
     <>
       <Wrapper>
-        <button onClick={onClick}>Click</button>
         <AnimatePresence>
-          {showing ? (
-            <Box
-              initial="initial"
-              animate="visible"
-              exit="leaving"
-              variants={boxVariants}
-            />
-          ) : null}
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(i =>
+            i === visible ? (
+              <Box
+                variants={boxVariants}
+                initial="initial"
+                animate="visible"
+                exit="leaving"
+                key={i}
+              >
+                {i}
+              </Box>
+            ) : null
+          )}
         </AnimatePresence>
+        <button onClick={nextPlease}>next</button>
+        <button onClick={prevPlease}>prev</button>
       </Wrapper>
     </>
   );
