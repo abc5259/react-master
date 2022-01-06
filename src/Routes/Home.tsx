593e943aad5fdab3f1fc369079792c8d3f5504f3
LeeJaeHoon <dlwogns3413@naver.com>
2022-01-07 00:02:34 +0900

    feat: slider part one

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,3 +1,5 @@
+import { AnimatePresence, motion } from "framer-motion";
+import { useEffect, useState } from "react";
 import { useQuery } from "react-query";
 import styled from "styled-components";
 import { getMovies, IGetMoviesResult } from "../api";
@@ -37,22 +39,94 @@ const OverView = styled.p`
   width: 50%;
 `;
 
+const Slider = styled.div`
+  position: relative;
+  top: -200px;
+`;
+const Row = styled(motion.div)`
+  display: grid;
+  gap: 10px;
+  grid-template-columns: repeat(6, 1fr);
+  position: absolute;
+  width: 100%;
+`;
+const Box = styled(motion.div)`
+  background-color: white;
+  height: 200px;
+  color: red;
+  font-size: 64px;
+`;
+
+const rowVariants = {
+  hidden: (innerWidth: number) => {
+    return {
+      x: innerWidth + 10,
+    };
+  },
+  visible: {
+    x: 0,
+    transition: {
+      type: "tween",
+      duration: 1,
+    },
+  },
+  exit: (innerWidth: number) => {
+    return {
+      x: -innerWidth - 10,
+      transition: {
+        type: "tween",
+        duration: 1,
+      },
+    };
+  },
+};
+
 const Home = () => {
   const { isLoading, data } = useQuery<IGetMoviesResult>(
     ["movies", "nowPlaying"],
     getMovies
   );
-  console.log(isLoading, data);
+  const [index, setIndex] = useState(0);
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+  const incraseIndex = () => setIndex(prev => prev + 1);
+  const handleResize = () => {
+    setInnerWidth(window.innerWidth);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [innerWidth]);
   return (
     <Wrapper>
       {isLoading ? (
         <Loader>Loading...</Loader>
       ) : (
         <>
-          <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
+          <Banner
+            onClick={incraseIndex}
+            bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}
+          >
             <Title>{data?.results[0].title}</Title>
             <OverView>{data?.results[0].overview}</OverView>
           </Banner>
+          <Slider>
+            <AnimatePresence custom={innerWidth}>
+              <Row
+                custom={innerWidth}
+                variants={rowVariants}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                key={index}
+              >
+                {[1, 2, 3, 4, 5, 6].map(i => (
+                  <Box key={i}>{i}</Box>
+                ))}
+              </Row>
+            </AnimatePresence>
+          </Slider>
         </>
       )}
     </Wrapper>
